45b70539793939b00bd61b62cd9cd0d7cca37b8e
bbmseries-maxn <bbmseries@gmail.com>
2026-07-24 12:38:06 +0300

    Fix: 3 themes

--- src/app/features/inventory/inventory.service.ts
+++ src/app/features/inventory/inventory.service.ts
@@ -2,7 +2,6 @@ import { Injectable, inject, signal, computed } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product, Category, Supplier } from '../../shared/services/pos-data.models';
 import { SalesService } from '../../shared/services/sales';
-import { ThemeService } from '../../shared/services/theme.service';
 
 @Injectable({
   providedIn: 'root'
